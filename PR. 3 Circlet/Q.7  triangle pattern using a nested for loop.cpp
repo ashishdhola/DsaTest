179b13b3e14ebea840e8516787aef07539c6179b
Ashish <ashishdhola0.7@gmail.com>
2024-12-21 18:12:54 +0530

    Q_7 updte

--- PR. 3 Circlet/Q.7  triangle pattern using a nested for loop.cpp
+++ PR. 3 Circlet/Q.7  triangle pattern using a nested for loop.cpp
@@ -10,12 +10,12 @@ using namespace std;
 
 int main() 
 {
-    int n=5; 
+    int n=5 ; 
     
     for(int i=1;i<=n;i++) 
     {
        
-        for(int j=1;j<=n-i;j++) 
+        for(int k=1;k<=n-i;k++) 
         {
             cout << " ";  
         }
